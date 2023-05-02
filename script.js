0e749eb6d738d6a206203e88b52b2e88c7fbd6df
JordanMorrisDev <jordanandrewmorris12@gmail.com>
2023-05-02 03:36:36 +0000

    Functioning model on test data

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
-import { TRAINING_DATA } from 'https://storage.googleapis.com/jmstore/TensorFlowJS/EdX/TrainingData/real-estate-data.js';
 
+////////////IMPORT TRANING DATA
+import { TRAINING_DATA } from 'https://storage.googleapis.com/jmstore/TensorFlowJS/EdX/TrainingData/real-estate-data.js';
 //Input feature pairs (House size, number of bedrooms)
 const INPUTS = TRAINING_DATA.inputs;
-
 //Current listed house prices in dollars given their features above
 const OUTPUTS = TRAINING_DATA.outputs;;
-
 //Shuffle the two arrays in the same way so inputs still match outputs indexes
 tf.util.shuffleCombo(INPUTS, OUTPUTS)
 
-//Create Tensors
+////////CREATE TENSORS
 const INPUTS_TENSOR = tf.tensor2d(INPUTS);
 const OUTPUTS_TENSOR = tf.tensor1d(OUTPUTS);
 
 //Normalise Tensors
 const FEATURE_RESULTS = normalize(INPUTS_TENSOR);
-
 console.log('Normalized Values:')
 FEATURE_RESULTS.NORMALIZED_VALUES.print();
 console.log('Min Values:')
@@ -23,6 +21,7 @@ FEATURE_RESULTS.MIN_VALUES.print();
 console.log('Max values:')
 FEATURE_RESULTS.MAX_VALUES.print();
 
+//Dispose of unneeded Tensors.
 INPUTS_TENSOR.dispose();
 
 //Normalize inputs to 0..1
@@ -45,4 +44,59 @@ function normalize(tensor, min, max) {
 		return { NORMALIZED_VALUES, MIN_VALUES, MAX_VALUES };
 	});
 	return result;
+}
+
+///////CREATE MODEL ARCHITECTURE
+const model = tf.sequential();
+
+//one dense layer, 1 neuron and input of 2 features
+model.add(tf.layers.dense({
+	inputShape: [2],
+	units: 1,
+}));
+
+model.summary();
+
+train();
+
+async function train() {
+	const LEARNING_RATE = 0.01;
+
+	//Compile model with the learning rate and specify a loss function to use
+	model.compile({
+		optimizer: tf.train.sgd(LEARNING_RATE),
+		loss: 'meanSquaredError'
+	});
+
+	//Do the training
+	let results = await model.fit(FEATURE_RESULTS.NORMALIZED_VALUES, OUTPUTS_TENSOR, {
+		validationSplit: 0.15, //Use 15% of data for validation
+		shuffle: true, //Ensure data is shuffled
+		batchSize: 32, //Use batch sizes of 64
+		epochs: 20 //Go over the data 10 times
+	});
+
+	OUTPUTS_TENSOR.dispose();
+	FEATURE_RESULTS.NORMALIZED_VALUES.dispose();
+
+	console.log('Average error loss: ' + Math.sqrt(results.history.loss[results.history.loss.length - 1]));
+	console.log('Average validation error loss: ' + Math.sqrt(results.history.val_loss[results.history.val_loss.length - 1]));
+
+	evaluate();
+}
+
+function evaluate() {
+	//predict answer for a single piece of data
+	tf.tidy(function() {
+		let newInput = normalize(tf.tensor2d([[750, 1]]), FEATURE_RESULTS.MIN_VALUES, FEATURE_RESULTS.MAX_VALUES
+		);
+		let output = model.predict(newInput.NORMALIZED_VALUES);
+		output.print();
+	});
+
+	FEATURE_RESULTS.MIN_VALUES.dispose();
+	FEATURE_RESULTS.MAX_VALUES.dispose();
+	model.dispose();
+
+	console.log(tf.memory().numTensors);
 }
